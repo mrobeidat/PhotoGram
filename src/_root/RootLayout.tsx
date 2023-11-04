7cfa1cd9d8896382a04b513239b03717b0622c69
mrobeidat <y.linux96@gmail.com>
2023-11-04 17:15:03 +0300

    File Structure + Routing + Sign up Page

---
+++ src/_root/RootLayout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RootLayout = () => {
+  return (
+    <h1>RootLayout</h1>
+  )
+}
+
+export default RootLayout
---
+++ src/_root/pages/index.tsx
@@ -0,0 +1,1 @@
+export {default as Hone} from './Home'
