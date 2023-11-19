598892fbc05414a10f98d3a7c9da41b813c909d6
mrobeidat <y.linux96@gmail.com>
2023-11-19 15:17:01 +0300

    Implement the post card functionality

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -26,7 +26,7 @@ const Home = () => {
             <ul className="flex flex-col flex-1 gap-9 w-full ">
               {posts?.documents.map((post: Models.Document) => (
                 <li key={post.$id} className="flex justify-center w-full">
-                  <PostCard />
+                  <PostCard post={post} key={post.caption} />
                 </li>
               ))}
             </ul>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,30 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(dateString: string): string {
+  const currentDate: Date = new Date();
+  const inputDate: Date = new Date(dateString);
+
+  const timeDifference: number = currentDate.getTime() - inputDate.getTime();
+  const seconds: number = Math.floor(timeDifference / 1000);
+  const minutes: number = Math.floor(seconds / 60);
+  const hours: number = Math.floor(minutes / 60);
+  const days: number = Math.floor(hours / 24);
+
+  if (days > 1) {
+      return `${days} days ago`;
+  } else if (days === 1) {
+      return '1 day ago';
+  } else if (hours > 1) {
+      return `${hours} hours ago`;
+  } else if (hours === 1) {
+      return '1 hour ago';
+  } else if (minutes > 1) {
+      return `${minutes} minutes ago`;
+  } else if (minutes === 1) {
+      return '1 minute ago';
+  } else {
+      return 'Just now';
+  }
+}
