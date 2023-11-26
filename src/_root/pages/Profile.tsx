042f26afc9ebaaad0150c18f926fd4e67cc978eb
mrobeidat <y.linux96@gmail.com>
2023-11-26 13:21:06 +0300

    fix posts count

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -64,7 +64,7 @@ const Profile = () => {
             </div>
 
             <div className="flex gap-8 mt-10 items-center justify-center xl:justify-start flex-wrap z-20">
-              <StatBlock value={currentUser.posts.length -1} label="Posts" />
+              <StatBlock value={currentUser.posts.length} label="Posts" />
               <StatBlock value={120} label="Followers" />
               <StatBlock value={320} label="Following" />
             </div>
