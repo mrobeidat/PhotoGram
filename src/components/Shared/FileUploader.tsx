9e61e3da11b9a4eff82bf43c23128106cf1b703d
mrobeidat <y.linux96@gmail.com>
2023-11-30 00:19:50 +0300

    style the file uploader component

--- src/components/Shared/FileUploader.tsx
+++ src/components/Shared/FileUploader.tsx
@@ -31,7 +31,7 @@ const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
                 fileUrl ? (
                     <>
                         <div className="flex flex-1 justify-center w-full p-5 lg:p-10">
-                            <img src={fileUrl} alt="image" className="file_uploader-img" />
+                            <img style={{ objectFit: "contain" }} src={fileUrl} alt="image" className="file_uploader-img" />
                         </div>
                         <p className="file_uploader-label">Click or drag photo to replace</p>
                     </>
