22ad1e479b47c21efb540e2c3b04bd8bc73f7849
mrobeidat <y.linux96@gmail.com>
2023-12-12 16:26:30 +0300

    add web analytics - vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import { QueryProvider } from "./lib/react-query/queryProvider";
+import { Analytics } from '@vercel/analytics/react';
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
         <QueryProvider>
             <AuthProvider>
                 <App />
+                <Analytics />
             </AuthProvider>
         </QueryProvider>
     </BrowserRouter>
