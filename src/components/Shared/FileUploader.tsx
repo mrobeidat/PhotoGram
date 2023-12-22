d962d4deb9be046792fdbd7781fe877cd70c3604
mrobeidat <y.linux96@gmail.com>
2023-12-22 19:32:58 +0300

    heic

--- src/components/Shared/FileUploader.tsx
+++ src/components/Shared/FileUploader.tsx
@@ -1,8 +1,8 @@
 import { useCallback, useState } from "react";
 import { FileWithPath, useDropzone } from "react-dropzone";
+import heic2any from "heic2any"; // Import heic2any library
+
 import { Button } from "@/components/ui/button";
-import { convertFileToUrl } from "@/lib/utils";
-import heic2any from "heic2any";
 
 type FileUploaderProps = {
   fieldChange: (files: File[]) => void;
@@ -15,25 +15,21 @@ const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
 
   const onDrop = useCallback(
     async (acceptedFiles: FileWithPath[]) => {
-      const convertedFiles = await Promise.all(
-        acceptedFiles.map(async (file) => {
-          // Check if the file is HEIF
-          if (file.type === "image/heif") {
-            // Convert HEIF to PNG
-            const convertedBlob = await heic2any({
-              blob: file,
-              toType: "image/png",
-            });
-            return new File([convertedBlob as BlobPart], `${file.name}.png`, {
-              type: "image/png",
-            });
-          }
-          return file;
-        })
-      );
+      const convertedFiles: File[] = [];
+
+      for (const file of acceptedFiles) {
+        if (file.type === "image/heic") {
+          // Convert HEIC to PNG using heic2any
+          const convertedFileBlob = await heic2any({ blob: file });
+          const convertedFile = new File([convertedFileBlob as Blob], file.name.replace(/\.[^/.]+$/, ".png"));
+          convertedFiles.push(convertedFile);
+        } else {
+          convertedFiles.push(file);
+        }
+      }
 
       setFile(convertedFiles);
-      setFileUrl(convertFileToUrl(convertedFiles[0]));
+      setFileUrl(URL.createObjectURL(convertedFiles[0]));
       fieldChange(convertedFiles);
     },
     [file]
@@ -41,48 +37,24 @@ const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
-    accept: {
-      "image/*": [".png", ".jpeg", ".jpg", ".heif"],
-    },
+    accept: { 'image/*': ['.heic', '.HEIF', '.png', '.jpg', '.jpeg', '.JPEG'] }
   });
 
   return (
-    <div
-      className="flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer"
-      {...getRootProps()}
-    >
-      <input
-        accept="image/*"
-        {...getInputProps()}
-        className="cursor-pointer"
-      />
+    <div className="flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer" {...getRootProps()}>
+      <input accept="image/*" {...getInputProps()} className="cursor-pointer" />
       {fileUrl ? (
         <>
           <div className="flex flex-1 justify-center w-full p-5 lg:p-10">
-            <img
-              style={{ objectFit: "contain" }}
-              src={fileUrl}
-              alt="image"
-              className="file_uploader-img"
-            />
+            <img style={{ objectFit: "contain" }} src={fileUrl} alt="image" className="file_uploader-img" />
           </div>
-          <p className="file_uploader-label">
-            Click or drag photo to replace
-          </p>
+          <p className="file_uploader-label">Click or drag photo to replace</p>
         </>
       ) : (
         <div className="file_uploader-box ">
-          <img
-            src="/assets/icons/file-upload.svg"
-            width={96}
-            height={77}
-            alt="file upload"
-          />
-          <h3 className="base-medium text-light-2 mb-2 mt-6">
-            Drag photo here
-          </h3>
+          <img src="/assets/icons/file-upload.svg" width={96} height={77} alt="file upload" />
+          <h3 className="base-medium text-light-2 mb-2 mt-6">Drag photo here</h3>
           <p className="text-light-4 small-regular mb-6">SVG, PNG, JPG</p>
-
           <Button type="button" className="shad-button_dark_4">
             Select from computer
           </Button>
