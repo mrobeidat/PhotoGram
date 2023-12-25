12a2028921bb177aeb84f270ee1b91ee41871db9
mrobeidat <y.linux96@gmail.com>
2023-12-25 08:55:19 +0300

    change toolbar color palette

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -112,11 +112,11 @@ const PostForm = ({ post, action }: PostFormProps) => {
         "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff",
         "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2",
         "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466",
-        "#1F75FE", "#00BFFF"
+        '#BF40BF', "#1F75FE", "#00BFFF",
     ];
 
     const toolbarOptions = [['bold', 'italic', 'underline'], ['link'],
-    [{ 'color': COLORS }, { 'background': COLORS }], [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+    [{ 'color': COLORS }, { 'background': COLORS }]
     ];
 
     // To check if there are any changes on the form so the update button will be enabled otherwise it will be disabled using "isDirty" property.
