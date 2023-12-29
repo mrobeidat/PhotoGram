612b14a6efdcff6653987fd3e865f8e54d9d83b4
mrobeidat <y.linux96@gmail.com>
2023-12-29 15:16:09 +0300

    fixes

--- src/components/Shared/PostCard.tsx
+++ src/components/Shared/PostCard.tsx
@@ -8,7 +8,7 @@ import { useEffect, useState } from "react"
 // import { useGetRecentPosts } from "@/lib/react-query/queriesAndMutations";
 import Loader from "./Loader"
 import { PhotoProvider, PhotoView } from "react-photo-view"
-import { isAndroid, isWindows, isMacOs } from 'react-device-detect';
+import { isAndroid, isWindows, isMacOs, isIOS } from 'react-device-detect';
 
 type PostCardProps = {
   post: Models.Document
@@ -201,21 +201,38 @@ const PostCard = ({ post }: PostCardProps) => {
               <>
                 {imageUrl && (
                   <div className="post_details-img object-cover !w-full !h-auto !p-0" style={{ position: 'relative', borderRadius: "10px" }}>
-                    <video
-                      id={`video-${post?.$id}`}
-                      autoPlay={isVideoPlaying}
-                      loop
-                      controls={false}
-                      onClick={handleTap}
-                      className="post-card_img"
-                      style={{
-                        width: '100%',
-                        borderRadius: '10px',
-                        boxShadow: 'rgba(17, 67, 98, 0.841) 0px 20px 30px -10px',
-                      }}
-                    >
-                      <source src={imageUrl} type="video/mp4" />
-                    </video>
+                    {isIOS ? (
+                      <video
+                        id={`video-${post?.$id}`}
+                        autoPlay={isVideoPlaying}
+                        loop
+                        controls
+                        className="post-card_img"
+                        style={{
+                          width: '100%',
+                          borderRadius: '10px',
+                          boxShadow: 'rgba(17, 67, 98, 0.841) 0px 20px 30px -10px',
+                        }}
+                      >
+                        <source src={imageUrl} type="video/mp4" />
+                      </video>
+                    ) : (
+                      <video
+                        id={`video-${post?.$id}`}
+                        autoPlay={isVideoPlaying}
+                        loop
+                        controls={false}
+                        onClick={handleTap}
+                        className="post-card_img"
+                        style={{
+                          width: '100%',
+                          borderRadius: '10px',
+                          boxShadow: 'rgba(17, 67, 98, 0.841) 0px 20px 30px -10px',
+                        }}
+                      >
+                        <source src={imageUrl} type="video/mp4" />
+                      </video>)}
+
                     <div
                       style={{
                         position: 'absolute',
