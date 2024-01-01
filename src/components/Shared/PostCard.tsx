c3c6710012db47c3d206dc3ce6408487cf2368ec
mrobeidat <y.linux96@gmail.com>
2024-01-01 11:20:48 +0300

    revert

--- src/components/Shared/PostCard.tsx
+++ src/components/Shared/PostCard.tsx
@@ -16,7 +16,7 @@ const PostCard = ({ post }: PostCardProps) => {
   const [contentType, setContentType] = useState('');
   const [isVideoPlaying, setIsVideoPlaying] = useState(false);
   const { user } = useUserContext();
-  const [isMuted, setIsMuted] = useState(false);
+  // const [isMuted, setIsMuted] = useState(false);
 
   if (!post.creator) return null;
 
@@ -81,12 +81,12 @@ const PostCard = ({ post }: PostCardProps) => {
   }, [imageUrl, post.$id, isVideoPlaying]);
 
 
-  const handleTap = () => {
-    const videoElement = document.getElementById(`video-${post.$id}`) as HTMLVideoElement;
-    // Toggle the mute state
-    videoElement.muted = !isMuted;
-    setIsMuted(!isMuted);
-  };
+  // const handleTap = () => {
+  //   const videoElement = document.getElementById(`video-${post.$id}`) as HTMLVideoElement;
+  //   // Toggle the mute state
+  //   videoElement.muted = !isMuted;
+  //   setIsMuted(!isMuted);
+  // };
 
   console.log(isVideoPlaying);
 
@@ -189,13 +189,13 @@ const PostCard = ({ post }: PostCardProps) => {
         ) : (
           <div style={{ position: 'relative', borderRadius: '25px' }}>
             {imageUrl && (
-              <div onClick={handleTap}
+              <div
                 className="post_details-img object-cover !w-full !h-auto !p-0" style={{ position: 'relative', borderRadius: "10px" }}>
                 <video
                   id={`video-${post?.$id}`}
                   autoPlay={isVideoPlaying}
                   loop
-                  // controls={true}
+                  controls={true}
                   className="post-card_img"
                   style={{
                     width: '100%',
@@ -213,12 +213,12 @@ const PostCard = ({ post }: PostCardProps) => {
                     cursor: 'pointer',
                   }}
                 >
-                  {isMuted ? (
+                  {/* {isMuted ? (
                     <img height={21} width={21} src="/assets/icons/mute.png" alt="Mute" />
                   ) : (
                     <img height={22} width={22} src="/assets/icons/volume.png" alt="Unmute" />
                   )
-                  }
+                  } */}
                 </div>
               </div>
             )}
