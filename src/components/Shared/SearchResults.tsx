c014d5faf514bc1962604c93760729c1aaa2a8b0
Yousef Obeidat <y.linux96@gmail.com>
2024-07-04 23:40:20 +0300

    fixes

--- src/components/Shared/SearchResults.tsx
+++ src/components/Shared/SearchResults.tsx
@@ -2,21 +2,22 @@ import { Models } from "appwrite";
 import Loader from "./Loader";
 import GridPostList from "./GridPostsList";
 
-type searchResultsProps = {
+type SearchResultsProps = {
   isSearchFetching: boolean;
-  searchedPosts: Models.Document[]
-}
+  searchedPosts: Models.Document[];
+  filter: 'week' | 'month' | 'year';
+};
 
-const SearchResults = ({ isSearchFetching, searchedPosts }: searchResultsProps) => {
-  if (isSearchFetching) return <Loader />
+const SearchResults = ({ isSearchFetching, searchedPosts, filter }: SearchResultsProps) => {
+  if (isSearchFetching) return <Loader />;
   if (searchedPosts && searchedPosts.length > 0) {
     return (
-      <GridPostList posts={searchedPosts} />
-    )
+      <GridPostList posts={searchedPosts} filter={filter} />
+    );
   }
   return (
-    <p className="tex-light-4 mt-10 text-center w-full">No results found</p>
-  )
-}
+    <p className="text-light-4 mt-10 text-center w-full">No results found</p>
+  );
+};
 
-export default SearchResults
+export default SearchResults;
