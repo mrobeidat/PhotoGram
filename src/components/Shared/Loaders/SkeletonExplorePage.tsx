7a91d8302d241ec917d7c8996066465188c06df1
mrobeidat <y.linux96@gmail.com>
2024-07-27 18:56:03 +0300

    loader enhancements

--- src/components/Shared/Loaders/SkeletonExplorePage.tsx
+++ src/components/Shared/Loaders/SkeletonExplorePage.tsx
@@ -1,21 +1,21 @@
 export const SkeletonSearchBar = () => {
   return (
-    <div className="flex items-center gap-1 px-4 w-full mt-5 rounded-lg bg-gray-300 animate-pulse h-12">
-      <div className="w-6 h-6 bg-gray-300 rounded-full"></div>
-      <div className="w-full h-6 bg-gray-300 rounded"></div>
+    <div className="flex items-center gap-1 px-4 w-full mt-5 rounded-lg bg-gray-400 animate-pulse h-12">
+      <div className="w-6 h-6 bg-gray-400 rounded-full"></div>
+      <div className="w-full h-6 bg-gray-400 rounded"></div>
     </div>
   );
 };
 
 export const SkeletonDropdown = () => {
   return (
-    <div className="w-36 h-14 bg-gray-300 rounded-lg animate-pulse ml-auto"></div>
+    <div className="w-36 h-14 bg-gray-400 rounded-lg animate-pulse ml-auto"></div>
   );
 };
 
 export const SkeletonGridPost = () => {
   return (
-    <div className="relative w-full h-80 sm:w-46 sm:h-72 bg-gray-300 animate-pulse rounded-lg mb-4">
+    <div className="relative w-full h-80 sm:w-46 sm:h-72 bg-gray-400 animate-pulse rounded-lg mb-4">
       <div className="absolute bottom-2 left-2 flex items-center">
         <div className="h-6 w-6 bg-gray-400 rounded-full mr-2"></div>
         <div className="h-6 w-24 bg-gray-400 rounded"></div>
--- src/components/Shared/Loaders/SkeletonSavedPost.tsx
+++ src/components/Shared/Loaders/SkeletonSavedPost.tsx
@@ -1,10 +1,10 @@
 const SkeletonSavedPost = () => {
     return (
       <div className="p-4 animate-pulse">
-        <div className="h-48 bg-gray-300 rounded-lg mb-4"></div>
+        <div className="h-48 bg-gray-400 rounded-lg mb-4"></div>
         <div className="flex items-center">
-          <div className="h-6 w-6 bg-gray-300 rounded-full mr-2"></div>
-          <div className="h-6 w-24 bg-gray-300 rounded"></div>
+          <div className="h-6 w-6 bg-gray-400 rounded-full mr-2"></div>
+          <div className="h-6 w-24 bg-gray-400 rounded"></div>
         </div>
       </div>
     );
