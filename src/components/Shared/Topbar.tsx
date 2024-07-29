f2848796d175f5bee34327274d095b73e7e0bee4
mrobeidat <y.linux96@gmail.com>
2024-07-29 21:52:00 +0300

    bottom and topbar enhancements

--- src/components/Shared/Topbar.tsx
+++ src/components/Shared/Topbar.tsx
@@ -15,7 +15,7 @@ const Topbar = () => {
 
   return (
     <section className="topbar">
-      <div className="flex-between py-4 px-5">
+      <div className="flex-between py-2.5 px-5">
         <Link to="/" className="flex gap-3 items-center">
           <img
             className="pointer-events-none select-none"
